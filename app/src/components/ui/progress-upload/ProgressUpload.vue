c027d273f919297cf6d29c3bd271e1d0cea5411c
kolhov <kolhovv@ya.ru>
2025-04-07 02:51:26 +0200

    fix upload bar appearance

--- app/src/components/ui/progress-upload/ProgressUpload.vue
+++ app/src/components/ui/progress-upload/ProgressUpload.vue
@@ -10,10 +10,10 @@ const {loadingFiles} = storeToRefs(useFileUploader());
 <template>
   <div v-for="(file, index) in loadingFiles"
        :key="index"
-       class="flex sm:flex-row py-2 flex-col justify-between items-center border-y-[1px]"
+       class="flex sm:flex-row py-4 px-2 pr-4 flex-col justify-between items-center border-y-[1px]"
   >
     <span class="sm:w-1/3 flex flex-row justify-start items-center pl-2">
-      <Icon :icon="file.icon" class="text-lg" />
+      <Icon :icon="file.icon" class="text-lg flex-shrink-0" />
       <span class="px-2 text-sm">{{file.name}}</span>
     </span>
     <div class="sm:w-2/3 pt-2 sm:pt-0 w-full max-w-80">
